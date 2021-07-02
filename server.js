49b2ad71f932614a2ddd862b4dba400af142183a
Victor Orlunda <vicorlunda@gmail.com>
2021-07-02 20:31:56 +0100

    import and setup cors

--- server.js
+++ server.js
@@ -4,9 +4,12 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
 
+const cors = require('cors');
+
 // middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 
 // routes
 app.use('/api', require('./routes/datetime-routes'));
